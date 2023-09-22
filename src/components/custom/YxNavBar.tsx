c793e0bfbbf5778029f59f8f1019d5d7f3e017f9
YOUDEXSOF <yousofh255@gmail.com>
2023-09-22 13:27:18 +0330

    fixed download cv button bug on android

--- src/components/custom/YxNavBar.tsx
+++ src/components/custom/YxNavBar.tsx
@@ -3,6 +3,9 @@ import YxLogo from "./YxLogo";
 import YxButton from "./YxButton";
 import icon from "../../assets/icons/menu.svg";
 import { useYxStore } from "../../store";
+import useLanguageValue from "../../hooks/useLanguageValue";
+import cv_fa from "../../../cvs/cv-fa.pdf";
+import cv_en from "../../../cvs/cv-en.pdf";
 
 const YxNavBar = () => {
   const { setShowDrawer } = useYxStore();
@@ -10,6 +13,7 @@ const YxNavBar = () => {
     "rgba(0, 0, 0, 0.05);",
     "rgba(255,255,255,0.05)"
   );
+  const cv = useLanguageValue(cv_fa, cv_en);
 
   const borderColor = useColorModeValue(
     "rgba(0,0,0,0.1)",
@@ -40,6 +44,7 @@ const YxNavBar = () => {
             faTitle="PDF دانلود رزومه بصورت"
             enTitle="Donwload PDF Resume"
             download
+            to={cv}
           />
           <YxButton icon={icon} onClick={() => setShowDrawer(true)} />
         </HStack>
